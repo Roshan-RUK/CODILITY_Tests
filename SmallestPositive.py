4de514112d0330fde5cf8598311bf5703ba7f778
Roshan-RUK <roshanullakhan@gmail.com>
2023-02-02 23:18:42 +0530

    Program to find smallest Positive num not present in the lis

---
+++ SmallestPositive.py
@@ -0,0 +1,31 @@
+def solution(A):
+    flag = False
+    B = []
+    for i in range(0,len(A)):
+        if A[i] > 0:
+            B.append(A[i])
+            flag = True
+    B= sorted(B)
+    count = 0
+    smallPos = 1
+    maxLimit = len(B)
+    for count in range(1,maxLimit):
+        if flag == True:
+            if count not in B:
+                smallPos = count
+                break
+            elif count == maxLimit:
+                smallPos = count + 1
+            else:
+                smallPos = 1
+                count = count + 1
+    if count == maxLimit:
+        smallPos = count +1
+    return smallPos
+
+A = [11, 2, 6, 4, 3, 1]
+#A = [1,2,3]
+#A = [-1,-2,-3]
+solution(A)
+found = solution(A)
+print(found)
