1c8aaebb240b7e65de7b583805e9d5830c43fc0f
Roshan-RUK <roshanullakhan@gmail.com>
2023-02-10 00:14:26 +0530

    practice Python programs

--- practice_interview.py
+++ practice_interview.py
@@ -1,3 +1,55 @@
+#Program to replace string space with given character
+str1 = "ques ol"
+ch = 's'
+result = ""
+for i in str1:
+    if i == ' ':
+        i = ch
+    result = result + i
+print("Replaced string "+result)
+
+#Program to check given character vowel or consonant
+ch = 'p'
+if ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u':
+    print("Given character is vowel")
+else:
+    print("Given character is consonant")
+
+#Check if a string is palindrome or not
+str1 = "level"
+str2 = ""
+
+for i in str1:
+    str2 = i + str2
+
+if str1 == str2:
+    print("String is a palindrome")
+else:
+    print("String is not a palindrome")
+
+
+#Check if two strings are anagram
+str1 = "earth"
+str2 = "heart"
+if len(str1) == len(str2):
+    if sorted(str1) == sorted(str2):
+        print("Strings are Anagram")
+    else:
+        print("Strings are not anagram")
+else:
+    print("The strings are not of same size and it is not an anagram")
+
+#Count the occurance of a given character in a string
+str1 = "This is a string"
+ch = 'i'
+count =0
+for i in str1:
+    if i == ch:
+        count += 1
+print("{}{}{}{}".format("The number of ",ch," Occurance is ", count))
+
+
+
 #Return greatest of 3 numbers using lambda
 a,b,c = 32, 41, 11
 greatest = lambda a, b, c:a if(a > b and a > c) else (b if(b > c) else c)
